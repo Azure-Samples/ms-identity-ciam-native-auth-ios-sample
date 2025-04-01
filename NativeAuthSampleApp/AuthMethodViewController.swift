cec5291c56f68531b2f6f95cb09a9c9c7b59d2af
Yuki-YuXin <yuxin@microsoft.com>
2025-04-01 14:43:41 +0100

    Update the VC accordingly

--- NativeAuthSampleApp/AuthMethodViewController.swift
+++ NativeAuthSampleApp/AuthMethodViewController.swift
@@ -25,24 +25,19 @@
 import UIKit
 import MSAL
 
-class AuthMethodViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class AuthMethodViewController: UIViewController {
     var onSubmit: ((_ authMethod: MSALAuthMethod?, _ verificationContact: String?) -> Void)?
     var onCancel: (() -> Void)?
     
-    @IBOutlet weak var authMethodPicker: UIPickerView!
+    @IBOutlet weak var authMethodLabel: UILabel!
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var challengeTypeLabel: UILabel!
     
     var authMethods: [MSALAuthMethod] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-            
-        authMethodPicker.delegate = self
-        authMethodPicker.dataSource = self
-            
-        // Hide error label initially
+        
         errorLabel.isHidden = true
         
         // Update UI based on available auth methods
@@ -52,69 +47,10 @@ class AuthMethodViewController: UIViewController, UIPickerViewDelegate, UIPicker
     // Helper method to update UI based on available auth methods
     private func updateUIForAuthMethods() {
         if authMethods.isEmpty {
-            // If no auth methods available, hide picker and show text field only
-            authMethodPicker.isHidden = true
-            emailTextField.placeholder = "Enter your email address as the auth method"
+            authMethodLabel.text = "No default auth method."
         } else {
-            // If auth methods are available, show picker
-            authMethodPicker.isHidden = false
-            authMethodPicker.reloadAllComponents()
-            
-            // Select first method by default and update text field
-            if !authMethods.isEmpty {
-                authMethodPicker.selectRow(0, inComponent: 0, animated: false)
-                updateTextFieldForSelectedMethod(row: 0)
-            }
+            authMethodLabel.text = authMethods[0].loginHint
         }
-    }
-    
-    // Helper to update text field based on selected auth method
-    private func updateTextFieldForSelectedMethod(row: Int) {
-        guard row < authMethods.count else { return }
-        
-        let method = authMethods[row]
-        switch method.challengeType {
-        case "email":
-            challengeTypeLabel.text = "Email"
-            emailTextField.text = method.loginHint
-            emailTextField.keyboardType = .emailAddress
-        case "sms":
-            challengeTypeLabel.text = "SMS"
-            emailTextField.text = method.loginHint
-            emailTextField.keyboardType = .phonePad
-        default:
-            emailTextField.placeholder = "Unknown"
-            emailTextField.text = method.loginHint
-            emailTextField.keyboardType = .default
-        }
-    }
-    
-    // MARK: - UIPickerViewDataSource
-
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1 // One column
-    }
-
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return authMethods.count
-    }
-
-    // MARK: - UIPickerViewDelegate
-
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        guard row < authMethods.count else {
-            return nil
-        }
-        
-        // Display the challenge type with the first letter capitalized
-        return authMethods[row].challengeType.capitalized
-    }
-
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        updateTextFieldForSelectedMethod(row: row)
-        
-        // Clear any previous errors
-        errorLabel.isHidden = true
     }
     
     @IBAction func cancelPressed(_ sender: Any) {
@@ -125,35 +61,13 @@ class AuthMethodViewController: UIViewController, UIPickerViewDelegate, UIPicker
     }
     
 
-    @IBAction func submitPressed(_ sender: Any) {
-        // If auth methods list is empty, just use whatever the user entered
-        if authMethods.isEmpty {
-            if let inputText = emailTextField.text, !inputText.isEmpty {
-                // Cannot access MSALAuthMethod
-                onSubmit?(nil, emailTextField.text)
-                dismiss(animated: true)
-            } else {
-                errorLabel.text = "Please enter an authentication method"
-                errorLabel.isHidden = false
-            }
-            return
-        }
-        
-        // Get the selected authentication method if picker is visible
-        let selectedRow = authMethodPicker.selectedRow(inComponent: 0)
-        
-        if selectedRow >= 0 && selectedRow < authMethods.count {
-            let method = authMethods[selectedRow]
-            
-            if let userInput = emailTextField.text, !userInput.isEmpty {
-                onSubmit?(method, emailTextField.text)
-                dismiss(animated: true)
-            } else {
-                errorLabel.text = "Please enter the required information"
-                errorLabel.isHidden = false
-            }
+    @IBAction func continuePressed(_ sender: Any) {
+        if let userInput = emailTextField.text, !userInput.isEmpty {
+            let authMethod = authMethods.isEmpty ? nil : authMethods[0]
+            onSubmit?(authMethod, userInput)
+            dismiss(animated: true)
         } else {
-            errorLabel.text = "Please select an authentication method"
+            errorLabel.text = "Please enter the email as the authentication method registration."
             errorLabel.isHidden = false
         }
     }
